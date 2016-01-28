e72fd03d83af7da83c86eaeaa3ab22174ace84d6
Rudymil <Rudymil@users.noreply.github.com>
2016-01-29 00:35:43 +0100

    Create CSegment.cpp

---
+++ CSegment.cpp
@@ -0,0 +1,17 @@
+/*
+ * CSegment.cpp
+ *
+ *  Created on: 22 déc. 2009
+ *      Author: girouxi
+ */
+
+#include "CSegment.h"
+
+CSegment::CSegment() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CSegment::~CSegment() {
+	// TODO Auto-generated destructor stub
+}
